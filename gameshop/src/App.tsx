b256aed850b2b25c6ab562d265799639a6a71105
monik <Maalkolm>
2023-04-14 23:17:11 +0200

    GameShop

---
+++ gameshop/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Games from "./components/Games";
+import "./components/SingleGame";
+
+function App() {
+  return (
+    <div className="App">
+      <Games/>
+    </div>
+  );
+}
+
+export default App;
